95fa437f506f85febf20b36d22253464f690cfa4
abc <joel.rajesh13@gmail.com>
2021-08-04 18:43:36 +0530

    Assignment 1

--- block-BNaaeh/api-book/app.js
+++ block-BNaaeh/api-book/app.js
@@ -3,9 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sassMiddleware = require("node-sass-middleware");
+var mongoose = require("mongoose");
+var Book = require('./models/Book');
+var Comment = require('./models/Comment');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var booksRouter = require("./routes/books");
+var commentRouter = require("./routes/comment");
+
+mongoose.connect(
+  'mongodb://localhost/bookStore-api',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log(err ? err : 'Connected to database');
+  }
+);
 
 var app = express();
 
@@ -17,10 +30,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true,
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/v1/books", booksRouter);
+app.use('/api/v2/books', commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
